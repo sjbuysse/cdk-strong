373251a950b61b1586259fd6e5406c501aa3652b
Brecht Billiet <billietbrecht@gmail.com>
2019-11-08 08:19:40 +0100

    forms for add playlist

--- musicplayer/src/app/containers/create-playlist/create-playlist.component.ts
+++ musicplayer/src/app/containers/create-playlist/create-playlist.component.ts
@@ -1,21 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
-import {SpotifyService} from '../../services/spotify.service';
-import {Router} from '@angular/router';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SpotifyService } from '../../services/spotify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sb-create-playlist',
   template: `
     <form [formGroup]="form" (ngSubmit)="submit()">
-      <div>
-        <input formControlName="name" type="text" placeholder="Name">
-      </div>
-      <div>
-        <textarea formControlName="description" placeholder="Description"></textarea>
-      </div>
-      <div> public <input type="checkbox" formControlName="public"/></div>
-      <a routerLink="/" mat-flat-button>Cancel</a>
-      <button mat-flat-button color="primary" [disabled]="form.invalid">Create</button>
+      <mat-card>
+        <mat-card-title>Create new playlist</mat-card-title>
+        <mat-card-subtitle>In this form we can create brand new playlists</mat-card-subtitle>
+        <mat-card-content>
+          <mat-form-field>
+            <input formControlName="name" matInput type="text" placeholder="Name">
+          </mat-form-field>
+          <mat-form-field>
+            <textarea formControlName="description" matInput placeholder="Description"></textarea>
+          </mat-form-field>
+          <mat-checkbox formControlName="public">Public</mat-checkbox>
+        </mat-card-content>
+        <mat-card-actions>
+          <a routerLink="/" mat-flat-button>Cancel</a>
+          <button mat-flat-button color="primary" [disabled]="form.invalid">Create</button>
+        </mat-card-actions>
+      </mat-card>
     </form>
   `,
   styleUrls: ['./create-playlist.component.scss']
@@ -37,7 +45,7 @@ export class CreatePlaylistComponent {
   submit() {
     this.spotifyService.createPlaylist(this.form.value).subscribe(response => {
       this.router.navigate(['playlists', response.id]);
-    })
+    });
   }
 
 }
